26154483b7794a4ef468ff8ff35957109d1f92d9
luizmiele <luiz.miele@hotmail.com>
2024-08-06 01:03:03 -0300

    feat: Finaliza primeiro curso C# alura

--- Curso1AluraCsharp/Aula3/Program.cs
+++ Curso1AluraCsharp/Aula3/Program.cs
@@ -1,3 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//1 - Escrever uma função que a partir de dois números de ponto flutuante a e b exiba no console o resultado de suas quatro operações básicas 
+//(adição, subtração, divisão e multiplicação), utilizando interpolação de strings.
+string CalculoDoisNumeros()
+{
+    Console.WriteLine("Digite o primeiro numero: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    
+    Console.WriteLine("Digite o segundo numero: ");
+    int b = Convert.ToInt32(Console.ReadLine());
+    
+    Console.Clear();
+    int soma = a + b;
+    int subtracao = a - b;
+    int multiplicacao = a * b;
+    int divisao = a / b;
+    
+    return $"Soma de {a} + {b} é: {soma}\nSubratacao de {a} - {b} é: {subtracao}\nMultiplicação de {a} * {b} é: {multiplicacao}\nDivisao de {a} / {b} é: {divisao}" ;
+}
 
-Console.WriteLine("Hello, World!");
+// Console.WriteLine(CalculoDoisNumeros());
+
+
+//2- Criar uma lista de bandas vazia e adicionar suas bandas prediletas em seguida.
+// 3- Utilizar a estrutura 'for' para mostrar todas as suas bandas preferidas, listadas na lista do exercício anterior, no console.
+List<string> bandas = new List<string> { };
+
+void AdicionaBandas()
+{
+    bool sair = false;
+    do
+    {
+        Console.WriteLine("Digite o nome de uma banda. ou digite SAIR");
+        var banda = Console.ReadLine()!;
+
+        if (banda == "SAIR" || banda == "sair" )
+        {
+            foreach (string bandaExibir in bandas)
+            {
+                Console.WriteLine(bandaExibir);
+            }
+            sair = true;
+        }
+        bandas.Add(banda);
+    } while(sair == false);
+}
+
+//AdicionaBandas();
+
+// 4- Criar um programa que calcula a soma de todos os elementos inteiros em uma lista.
+
+
+
+void somaDaLista()
+{
+    List<int> numerosRandom = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int somaNumeros = 0;
+    for (int i = 0; i < numerosRandom.Count; i++ )
+    {
+        somaNumeros += numerosRandom[i];
+    }
+    Console.WriteLine($"Resultado da soma da lista é: {somaNumeros}");
+}
+
+somaDaLista();
---
+++ Curso1AluraCsharp/Aula4/Program.cs
@@ -0,0 +1,130 @@
+﻿// 1- Criar um dicionário que represente um aluno, com uma lista de notas, e mostre a média de suas notas na tela.
+Dictionary<string, List<int>> alunos = new Dictionary<string, List<int>>
+{
+    {"Joao", new List<int>{ 10, 10, 9}},
+    {"Pedro", new List<int>{9, 4, 10}},
+    {"Paulo", new List<int> { 4, 3, 2}}
+};
+
+void MostraMediaAlunos()
+{
+    foreach (var aluno in alunos)
+    {
+        double soma = 0;
+        for(int i = 0; i < aluno.Value.Count ; i++){
+            soma += aluno.Value[i];
+        }
+        double media = soma / aluno.Value.Count();
+        Console.WriteLine($"A média do aluno {aluno.Key} é: {media}");
+    }
+}
+
+//MostraMediaAlunos();
+
+//2- Criar um programa que gerencie o estoque de uma loja. Utilize um dicionário para armazenar produtos e suas
+// quantidades em estoque e mostre, a partir do nome de um produto, sua quantidade em estoque. 
+
+Dictionary<string, int> produtos = new Dictionary<string, int>
+{
+    {"Mouse", 10},
+    {"Teclado", 14},
+    {"Fone", 2}
+};
+
+void MostrarEstoque()
+{
+    Console.WriteLine("Escolha um produto ");
+    foreach (var produto in produtos)
+    {
+        Console.WriteLine(produto.Key);
+    }
+    var opcao = Console.ReadLine();
+
+    if (produtos.ContainsKey(opcao))
+    {
+        Console.WriteLine($"O estoque de {opcao} é de {produtos[opcao]} unidades.");
+    }
+    else
+    {
+        Console.WriteLine("Produto não encontrado.");
+    }
+}
+
+//MostrarEstoque();
+
+// 3- Crie um programa que implemente um quiz simples de perguntas e respostas. Utilize um dicionário para armazenar as
+// perguntas e as respostas corretas.
+
+Dictionary<string, string> quiz = new Dictionary<string, string>
+{
+    {"Quantos Mouse tem na loja anterior?", "10"},
+    {"Quantos Teclados tem na loja anterior?", "14"},
+    {"Quantos Fones tem na loja anterior?", "2"}
+};
+
+void JogoQuiz()
+{
+    int pontuacao = 0;
+
+    foreach (var pergunta in quiz)
+    {
+        Console.WriteLine(pergunta.Key);
+        Console.Write("Sua resposta: ");
+        string respostaUsuario = Console.ReadLine();
+
+        if (respostaUsuario.ToLower() == pergunta.Value.ToLower())
+        {
+            Console.WriteLine("Resposta CERTA!!!!\n");
+            pontuacao++;
+        }
+        else
+        {
+            Console.WriteLine($"Incorreto. A resposta correta é: {pergunta.Value}\n");
+        }
+    }
+
+    Console.WriteLine($"Pontuação final: {pontuacao} de {quiz.Count}");
+}
+
+//JogoQuiz();
+
+// 4- Criar um programa que simule um sistema de login utilizando um dicionário para armazenar nomes de usuário e senhas.
+
+Dictionary<string, string> loginSenha = new Dictionary<string, string>
+{
+    {"Miele", "123"},
+    {"Raphael", "456"},
+    {"Boeck", "111"}
+};
+
+void Cadastro()
+{
+    Console.WriteLine("Digite o login que deseja criar: ");
+    var novoLogin = Console.ReadLine()!;
+    Console.WriteLine("Digte a senha: ");
+    var novaSenha = Console.ReadLine()!;
+
+    loginSenha.Add(novoLogin, novaSenha);
+}
+
+void Login()
+{
+    Console.WriteLine("Qual o seu login? ");
+    var login = Console.ReadLine()!;
+    
+    Console.WriteLine("Qual a sua senha? ");
+    var senha = Console.ReadLine()!;
+
+    if (loginSenha.ContainsKey(login) && loginSenha[login] == senha)
+    {
+        Console.WriteLine("LOGIN EFETUADO!");
+    }
+    else
+    {
+        Console.WriteLine("Login ou senha incorreto!");
+    }
+    
+}
+Cadastro();
+Login();
+
